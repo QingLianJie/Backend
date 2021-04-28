a873144b1eaf6d870a43f48dcf7350d5b381853a
bakapiano <bakapiano@outlook.com>
2021-04-28 18:25:41 +0800

    fix static files

--- qinglianjie/urls.py
+++ qinglianjie/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views import static ##新增
+from django.conf import settings ##新增
+from django.conf.urls import url ##新增
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('frontend.urls')),
+    url(r'^static/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static'),
 ]
